1bd5b5f527a11ba547e5834372bc0753de011c46
Rory Luo <camellre@gmail.com>
2023-04-17 16:54:51 -0700

    add sidebar shrink

--- src/routes/AccountPage/AccountPage.tsx
+++ src/routes/AccountPage/AccountPage.tsx
@@ -8,19 +8,43 @@ import { FileExclamationIcon } from "../../genericComponents/FileExclamationIcon
 import { FileCheckIcon } from "../../genericComponents/FileCheckIcon/FileCheckIcon";
 import { InboxIcon } from "../../genericComponents/InboxIcon/InboxIcon";
 import Button from "../../genericComponents/Button/Button";
+import { LeftIcon } from "../../genericComponents/LeftIcon/LeftIcon";
+import { useState } from "react";
 
 function AccountPage() {
+  const [sideBarExtend, setSiderBarExtend] = useState(true);
+
+  const handleSideBarTogglerClick = () => {
+    if (sideBarExtend) return setSiderBarExtend(false);
+    return setSiderBarExtend(true);
+  };
   return (
     <>
       <header>
         <NavigationBar />
       </header>
       <main className={style.accountBodyWrapper}>
-        <article className={style.sideBar}>
+        <article
+          className={
+            sideBarExtend
+              ? style.sideBar
+              : [style.sideBar, style.sideBarShrink].join(" ")
+          }
+        >
+          <LeftIcon
+            handleClick={handleSideBarTogglerClick}
+            leftIconStyle={
+              sideBarExtend
+                ? style.sideBarToggler
+                : [style.sideBarToggler, style.transformedToggler].join(" ")
+            }
+          />
           <div className={style.accountProfileBoxWrapper}>
             <UserIcon userIconStyle={style.userIcon} />
             <div>
-              <p>ID号/名字</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                ID号/名字
+              </p>
               <Link to={"/account/profile"}>
                 <p>账号管理</p>
               </Link>
@@ -29,25 +53,35 @@ function AccountPage() {
           <ul className={style.sideBarMenuWrapper}>
             <li className={style.sideBarMenuItem}>
               <FolderIcon folderIconStyle={style.sideBarMenuIcon} />
-              <p>我的申请</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                我的申请
+              </p>
             </li>
             <li className={style.sideBarMenuItem}>
               <FileExclamationIcon
                 fileExclamationIconStyle={style.sideBarMenuIcon}
               />
-              <p>未交申请</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                未交申请
+              </p>
             </li>
             <li className={style.sideBarMenuItem}>
               <FileCheckIcon fileCheckIconStyle={style.sideBarMenuIcon} />
-              <p>已交申请</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                已交申请
+              </p>
             </li>
             <li className={style.sideBarMenuItem}>
               <FilePlusIcon filePlusIconStyle={style.sideBarMenuIcon} />
-              <p>新建申请</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                新建申请
+              </p>
             </li>
             <li className={style.sideBarMenuItem}>
               <InboxIcon inboxIconStyle={style.sideBarMenuIcon} />
-              <p>收信箱</p>
+              <p className={sideBarExtend ? " " : style.sideBarShrinkText}>
+                收信箱
+              </p>
             </li>
           </ul>
           <Button text="登出账号" theme={style.logOutButton} />
