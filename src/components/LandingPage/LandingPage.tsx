bb8a32da1fdb4c6851b07ad563af1eb3b546d239
Rory Luo <camellre@gmail.com>
2023-04-21 12:01:23 -0700

    update landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -2,11 +2,12 @@ import Button from "../../genericComponents/Button/Button";
 import ImageWrapper from "../../genericComponents/ImageWrapper/ImageWrapper";
 import style from "./LandingPage.module.scss";
 import passport_1 from "../../assets/passport_1.webp";
+import passport_2 from "../../assets/passport_2.webp";
 
 export const LandingPage = () => {
   return (
     <article>
-      <ul>
+      <ul className={style.landingPageWrapper}>
         <li className={style.content_1_Background}>
           <div className={style.content_1_Wrapper}>
             <h2 className={style.content_1_text_1}>说走就走</h2>
@@ -25,7 +26,24 @@ export const LandingPage = () => {
             />
           </div>
         </li>
-        <li></li>
+        <li className={style.content_2_Background}>
+          <div className={style.content_2_BlackBackground}></div>
+          <div className={style.content_2_Wrapper}>
+            <Button theme={style.content_2_Button} text="立刻咨询" />
+            <h2 className={style.content_2_text_1}>
+              为家庭未来保驾护航，拥有第二公民身份
+            </h2>
+            <h2 className={style.content_2_text_2}>
+              为您的子女创造
+              <br />
+              无限的教育机会！
+            </h2>
+            <ImageWrapper
+              imageSrc={passport_2}
+              wrapperStyle={style.content_2_PassportImage}
+            />
+          </div>
+        </li>
         <li></li>
       </ul>
     </article>
