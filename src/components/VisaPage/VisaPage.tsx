0a4df96c45245906e5786274680689c229a4da58
Rory Luo <camellre@gmail.com>
2023-05-17 16:17:19 -0700

    update visa page

--- src/components/VisaPage/VisaPage.tsx
+++ src/components/VisaPage/VisaPage.tsx
@@ -1,7 +1,18 @@
 import style from "./VisaPage.module.scss";
+import visa_image_1 from "../../assets/visa_page_image_1.webp";
 
 const VisaPage = () => {
-  return <div>VisaPage</div>;
+  return (
+    <main className={style.visaPageGrid}>
+      <section>
+        <h1>签证代办</h1>
+        <p>便捷安全的签证代办服务</p>
+      </section>
+      <section>
+        <img src={visa_image_1} alt="" className={style.visaPageImage_1} />
+      </section>
+    </main>
+  );
 };
 
 export default VisaPage;
