6c01b0eb2d8257998d9ef8c67d09583493f9d241
Rory Luo <camellre@gmail.com>
2023-05-18 12:02:44 -0700

    update services pages

--- src/components/NotaryPage/NotaryPage.tsx
+++ src/components/NotaryPage/NotaryPage.tsx
@@ -1,7 +1,18 @@
 import style from "./NotaryPage.module.scss";
+import notary_image_1 from "../../assets/notary_page_1.webp";
 
 const NotaryPage = () => {
-  return <div>NotaryPage</div>;
+  return (
+    <main className={style.notaryPageGrid}>
+      <section>
+        <h1>加州公证服务</h1>
+        <p>诚信可靠的加州公证服务</p>
+      </section>
+      <section>
+        <img src={notary_image_1} alt="" className={style.notaryPageImage_1} />
+      </section>
+    </main>
+  );
 };
 
 export default NotaryPage;
--- src/components/OtherServices/OtherServices.tsx
+++ src/components/OtherServices/OtherServices.tsx
@@ -1,7 +1,22 @@
 import style from "./OtherServices.module.scss";
+import otherServices_image_1 from "../../assets/otherservice_image_1.webp";
 
 const OtherServices = () => {
-  return <div>OtherServices</div>;
+  return (
+    <main className={style.otherServicesPageGrid}>
+      <section>
+        <h1>其他服务</h1>
+        <p>便捷高效的在线服务</p>
+      </section>
+      <section>
+        <img
+          src={otherServices_image_1}
+          alt=""
+          className={style.otherServicesPageImage_1}
+        />
+      </section>
+    </main>
+  );
 };
 
 export default OtherServices;
