c550803bd5a0cae5f356c17d4704480c0c242e1d
Rory Luo <camellre@gmail.com>
2023-05-24 11:09:50 -0700

    update eligibility page

--- src/components/EligibilityCheck/EligibilityCheck.tsx
+++ src/components/EligibilityCheck/EligibilityCheck.tsx
@@ -10,33 +10,29 @@ export const EligibilityCheck = () => {
   return (
     <article className={style.backgroundWrapper}>
       <div className={style.background}>
-        <picture className={style.imageAll}>
+        <picture>
+          <img src={image_2} alt="" className={style.backgroundMobile} />
+        </picture>
+        <picture>
           <img src={image_1} alt="" />
         </picture>
-        <picture className={style.imageAll}>
-          <img src={image_2} alt="" />
-        </picture>
-        <picture className={style.imageAll}>
+        <picture>
           <img src={image_3} alt="" />
         </picture>
-        <picture className={style.imageAll}>
+        <picture>
           <img src={image_4} alt="" />
         </picture>
-        <picture className={style.imageAll}>
-          <img src={image_5} alt="" />
+        <picture>
+          <img src={image_5} alt="" className={style.backgroundMobile} />
         </picture>
       </div>
-      <div className={style.contentPositioner}>
-        <div className={style.contentWrapper}>
-          <h2 className={style.background_1}>
-            最适合您的移民
-            <br />
-            方案竟然是...?
-          </h2>
-          <Button theme={style.background_2} text="马上点击测试" />
-          <p className={style.background_3}>5分钟帮您匹配最合适的移民方案</p>
-        </div>
-      </div>
+      <h2 className={style.background_1}>
+        最适合您的移民
+        <br />
+        方案竟然是...?
+      </h2>
+      <Button theme={style.background_2} text="马上点击测试" />
+      <p className={style.background_3}>5分钟帮您匹配最合适的移民方案</p>
     </article>
   );
 };
